c1dcb6c607199619da9a4a342d1b24ac8733878a
hussein430 <hussen.alaa1222@gmail.com>
2025-06-12 10:25:48 +0300

    use reactive form instead of template driven form and add some enhance

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,9 +15,10 @@ import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
@@ -21,11 +28,41 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-  hide = signal(true);
+  loginForm: FormGroup;
+  hide = true;
+  private router = inject(Router);
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
   clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide());
+    this.hide = !this.hide;
     event.stopPropagation();
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login success', this.loginForm.value);
+      localStorage.setItem('token', 'dummy-token');
+      alert('✅ Login successful!');
+
+      setTimeout(() => {
+        this.router.navigate(['/dashboard']);
+      }, 2000);
+    } else {
+      console.log('Login failed', this.loginForm.errors);
+    }
   }
 }
